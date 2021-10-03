735d3c5a02cd9c20c422b86e1e6bf0cbc8e62063
nikit <nikit@LAPTOP-1VHB1HTQ>
2021-10-03 17:34:27 +0300

    Добавьте файлы проекта.

---
+++ Лаба 1 АЯ/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int x1, y1, x2, y2, x3, y3;
+	double AB, BC, CA;
+	double p;
+
+	cin >> x1 >> y1;
+	cin >> x2 >> y2;
+	cin >> x3 >> y3;
+
+
+	AB = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+	BC = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
+	CA = sqrt(pow(x1 - x3, 2) + pow(y1 - y3, 2));
+
+	p = (AB + BC + CA) / 2;
+	cout << sqrt(p * (p - AB) * (p - BC) * (p - CA));
+
+	return 0;
+} 
+
+
+
